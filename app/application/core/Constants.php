4be9e163f5b2f63da243b75c9d3ea5a91fa2391b
danisbagus <danisbagussetiawan@gmail.com>
2020-07-13 08:25:35 +0700

    create config file

--- app/application/core/Constants.php
+++
@@ -1,5 +0,0 @@
-<?php 
-
-define('BASEURL','http://localhost:4000');
-define('USERNAME','Danis Bagus');
-define('PROFESSION','Software Engineer');
--- app/application/init.php
+++ app/application/init.php
@@ -2,5 +2,7 @@
 
 # Call 2 the main file (class) as mvc maker. Use capslock because they are class
 require_once 'core/App.php';            
-require_once 'core/Controller.php';     
-require_once 'core/Constants.php';    
+require_once 'core/Controller.php';
+require_once 'core/Database.php';
+
+require_once 'config/config.php';
