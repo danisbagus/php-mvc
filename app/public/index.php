78a8cf5eaec2ee1f144b6fc5709278b19fa4a9fc
danisbagus <danisbagussetiawan@gmail.com>
2020-07-15 20:49:59 +0700

    start using session

--- app/public/index.php
+++ app/public/index.php
@@ -1,6 +1,9 @@
 <?php 
 
-# Call all needed file (Boostraping technique)
+// start session
+if ( !session_id() ) session_start();
+
+// Call all needed file (Boostraping technique)
 require_once '../application/init.php';     
 
 $app = new App;
