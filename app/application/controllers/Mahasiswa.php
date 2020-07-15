782d12311dc5960a06b26aab7ff21125107da878
danisbagus <danisbagussetiawan@gmail.com>
2020-07-15 20:50:41 +0700

    feature flash message

--- app/application/controllers/Mahasiswa.php
+++ app/application/controllers/Mahasiswa.php
@@ -23,6 +23,15 @@ class Mahasiswa extends Controller{
     public function tambah()
     {
         if( $this->model('Mahasiswa_model')->addDataMahasiswa($_POST) > 0) {
+
+            Flasher::setFlash('berhasil', 'ditambahkan', 'success');
+ 
+            header('Location: ' . BASEURL . '/mahasiswa');
+            exit;
+        } else {
+
+            Flasher::setFlash('gagal', 'ditambahkan', 'danger');
+ 
             header('Location: ' . BASEURL . '/mahasiswa');
             exit;
         }
